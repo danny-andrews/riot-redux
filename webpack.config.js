1c9b08b685a9059e32f8ca7572123df80e03e963
Danny Andrews <dannya@spiceworks.com>
2015-12-17 22:30:16 -0600

    Fix webpack.config to exclude node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
       }
     ]
